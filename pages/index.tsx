49bd7af8fb372af20d82c2d49ef5b0a8e1e1a53c
Lucas Moreira <lucas_10_moreira@hotmail.com>
2023-03-25 23:03:33 -0300

    feat: add initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <>
+      <PageContainerStyle>TA FUNCIONANDO</PageContainerStyle>
+    </>
+  );
+}
+
+export const PageContainerStyle = styled.div`
+  background: blue;
+`;
